8860c2844acc1c13f943d416008f0d6ca658db0c
Bailey Dalton <baileydalton007@gmail.com>
2023-09-30 16:43:04 -0400

    Finishes commenting on data

--- web_app/tab_screens/article_data_tab.py
+++ web_app/tab_screens/article_data_tab.py
@@ -5,17 +5,16 @@ def render(data):
     if data is None:
         return html.H3("Select a point on the plot to view data.")
 
-    if data['comments'] is None:
-        comments_list = []
-    else:
-        comments_list = data['comments'].split('\n')
-
+    comments_list = []
+    if data['comments'] is not None:
+        [comments_list.append(bullet) for bullet in data['comments'].split('\n') if bullet != ""]
+    
     return html.Div(id = 'tab', children=[
         html.H2(data['title'], style={'display': 'flex', 'justify-content': 'center'}),
         dcc.Link("Full Article", href=data['url'], target="_blank", style={'display': 'flex', 'justify-content': 'center', 'color': 'blue'}),
         html.Hr(),
 
-        html.Ul(children=[html.Li(bullet) for bullet in data['summary'].split('\n')]),
+        html.Ul(children=[html.Li(bullet.strip(" - ")) for bullet in data['summary'].split('\n') if bullet != ""]),
         
         html.H3('Commentary', style={'display': 'flex', 'justify-content': 'center'}),
         html.Hr(),
